72cfeeaefaa71abfea8368702e39b554510055e7
albxrtx <albertomariannechita@gmail.com>
2024-10-18 13:50:55 +0200

    password

--- scripts/register.js
+++ scripts/register.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   let nameInput = document.getElementById("name-input");
   let error = document.querySelector(".errorSpan");
-  let simbolos = /[!@#$%^&*()_+·~\-=\[\]{};':"\\|,.<>\/?]+/;
+  let simbolos = /[!@#$%^&*()_+·~\-=\[\]{};':"\\|,.<>\/?]/;
   let numeros = /[123456789]/;
+
   nameInput.addEventListener("input", function () {
     let inputValue = document.getElementById("name-input").value;
 
@@ -42,6 +43,41 @@ document.addEventListener("DOMContentLoaded", function () {
       nameInput.style.borderColor = "";
       nameInput.style.outlineColor = "";
       nameInput.style.color = "";
+    }
+  });
+
+  let passInput = document.getElementById("password-input");
+  let passError = document.querySelector(".passError");
+
+  passInput.addEventListener("input", function () {
+    let inputValue = document.getElementById("password-input").value;
+
+    if (inputValue.length <= 5) {
+      passError.textContent = "Contraseña muy corta";
+      passInput.style.borderColor = "red";
+      passInput.style.outlineColor = "red";
+      passInput.style.color = "red";
+    } else if (
+      inputValue.length > 5 &&
+      !simbolos.test(inputValue) &&
+      !numeros.test(inputValue)
+    ) {
+      passError.textContent = "La contraseña debe contener numeros o simbolos";
+      passInput.style.borderColor = "red";
+      passInput.style.outlineColor = "red";
+      passInput.style.color = "red";
+    } else {
+      passError.textContent = "";
+      passInput.style.borderColor = "";
+      passInput.style.outlineColor = "";
+      passInput.style.color = "";
+    }
+
+    if (inputValue == "") {
+      passError.textContent = "";
+      passInput.style.borderColor = "";
+      passInput.style.outlineColor = "";
+      passInput.style.color = "";
     }
   });
 });
