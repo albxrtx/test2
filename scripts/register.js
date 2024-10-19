944c8f1047f6aa32c9026e26a61dda9a031eb641
albxrtx <albertomariannechita@gmail.com>
2024-10-20 00:21:52 +0200

    register done

--- scripts/register.js
+++ scripts/register.js
@@ -1,77 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const simbolos = /[@#€_&\-+()/*·"':;!?.,~`|•√π÷×§∆£¥$¢^°={}%©®™✓[\]<>]/;
+  const numeros = /[123456789]/;
+  const letras = /[qwertyuiopasdfghjklñzxcvbnm]/;
+
+  // Declaración variables del campo nombre
   let nameInput = document.getElementById("name-input");
-  let error = document.querySelector(".errorSpan");
-  let simbolos = /[@#€_&\-+()/*·"':;!?.,~`|•√π÷×§∆£¥$¢^°={}%©®™✓[\]<>]/;
-  let numeros = /[123456789]/;
+  let nameError = document.querySelector(".errorSpan");
 
   nameInput.addEventListener("input", function () {
     let inputValue = document.getElementById("name-input").value;
-
-    if (inputValue.length <= 2) {
-      error.textContent = "Debe contener más de 2 caracteres";
-      nameInput.style.borderColor = "red";
-      nameInput.style.outlineColor = "red";
-      nameInput.style.color = "red";
-    } else if (inputValue.length >= 16) {
-      error.textContent = "Debe contener menos de 16 caracteres";
-      nameInput.style.borderColor = "red";
-      nameInput.style.outlineColor = "red";
-      nameInput.style.color = "red";
+    // Validacion del nombre
+    if (inputValue.length >= 2) {
+      if (simbolos.test(inputValue)) {
+        nameError.textContent = "Nombre no puede contener simbolos (!@#/&)";
+        nameInput.style.borderColor = "red";
+        nameInput.style.outlineColor = "red";
+        nameInput.style.color = "red";
+      } else if (numeros.test(inputValue)) {
+        nameError.textContent = "Nombre no puede contener numeros (0-9)";
+        nameInput.style.borderColor = "red";
+        nameInput.style.outlineColor = "red";
+        nameInput.style.color = "red";
+      } else {
+        nameError.textContent = "";
+        nameInput.style.borderColor = "";
+        nameInput.style.outlineColor = "";
+        nameInput.style.color = "";
+        nombreValido = true;
+      }
     } else {
-      error.textContent = "";
-      nameInput.style.borderColor = "";
-      nameInput.style.outlineColor = "";
-      nameInput.style.color = "";
-    }
-
-    if (simbolos.test(inputValue)) {
-      error.textContent = "Contiene simbolos (!·$%&/)";
+      nameError.textContent = "Nombre debe tener mas de 2 caracteres";
       nameInput.style.borderColor = "red";
       nameInput.style.outlineColor = "red";
       nameInput.style.color = "red";
     }
-
-    if (numeros.test(inputValue)) {
-      error.textContent = "Contiene números (0-9)";
-      nameInput.style.borderColor = "red";
-      nameInput.style.outlineColor = "red";
-      nameInput.style.color = "red";
-    }
-
+    //Limpiar por si nombre está vacío
     if (inputValue == "") {
-      error.textContent = "";
+      nameError.textContent = "";
       nameInput.style.borderColor = "";
       nameInput.style.outlineColor = "";
       nameInput.style.color = "";
     }
   });
 
+  // Declaración variables del campo contraseña
   let passInput = document.getElementById("password-input");
   let passError = document.querySelector(".passError");
 
   passInput.addEventListener("input", function () {
     let inputValue = document.getElementById("password-input").value;
-
-    if (inputValue.length <= 5) {
+    //Validación contraseña
+    if (inputValue.length >= 5) {
+      if (!letras.test(inputValue)) {
+        passError.textContent = "La contraseña debe contener letras (a-z)";
+      } else if (!numeros.test(inputValue)) {
+        passError.textContent = "La contraseña debe contener numeros (0-9)";
+      } else if (!simbolos.test(inputValue)) {
+        passError.textContent = "La contraseña debe contener simbolos (!@#/&)";
+      } else {
+        passError.textContent = "";
+        passInput.style.borderColor = "";
+        passInput.style.outlineColor = "";
+        passInput.style.color = "";
+        passValido = true;
+      }
+    } else {
       passError.textContent = "Contraseña muy corta";
       passInput.style.borderColor = "red";
       passInput.style.outlineColor = "red";
       passInput.style.color = "red";
-    } else if (
-      (inputValue.length > 5 && !simbolos.test(inputValue)) ||
-      !numeros.test(inputValue)
-    ) {
-      passError.textContent = "La contraseña debe contener numeros y simbolos";
-      passInput.style.borderColor = "red";
-      passInput.style.outlineColor = "red";
-      passInput.style.color = "red";
-    } else {
-      passError.textContent = "";
-      passInput.style.borderColor = "";
-      passInput.style.outlineColor = "";
-      passInput.style.color = "";
     }
-
+    //Limpiar por si contraseña está vacío
     if (inputValue == "") {
       passError.textContent = "";
       passInput.style.borderColor = "";
@@ -82,7 +81,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let botonEye = document.getElementById("toggle-password");
   botonEye.addEventListener("click", function () {
-    if (passInput.type == "password") passInput.type = "text";
-    else passInput.type = "password";
+    passInput.type = passInput.type === "password" ? "text" : "password";
   });
 });
